616845c38a2ecf193e32113889a504290cf993ec
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-18 11:42:21 +0900

    added buy function

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -4,14 +4,21 @@
     <div class="card-body">
       <div class="input-group mb-3">
         <input
-          type="text"
+          type="number"
           class="form-control"
           placeholder="Quantity"
           aria-label="Quantity"
           aria-describedby="button-addon2"
+          v-model="quantity"
         />
         <div class="input-group-append">
-          <button class="btn btn-outline-secondary" type="button" id="button-addon2">Buy</button>
+          <button
+            class="btn btn-success"
+            type="button"
+            id="button-addon2"
+            @click="buyStock"
+            :disabled="quantity <= 0"
+          >Buy</button>
         </div>
       </div>
     </div>
@@ -21,7 +28,23 @@
 <script>
 export default {
   name: "Stock",
-  props: ["stock"]
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockValue: this.stock.value,
+        stockQuantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    }
+  }
 };
 </script>
 
